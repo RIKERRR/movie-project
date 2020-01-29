9b2bdb9c329de0de456c2b091ff79d1199b99767
Justin Scott Riker <jsr.riker@gmail.com>
2020-01-28 21:48:01 -0700

    Add files via upload
    initial commit

---
+++ precache-manifest.1fd5daecea003063a257ed70934fa82c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ef9a98ad3b2cbe9431df497bf7a0261",
+    "url": "/movie-project/index.html"
+  },
+  {
+    "revision": "65b7accc5f31bbabf990",
+    "url": "/movie-project/static/css/main.93dd3d1f.chunk.css"
+  },
+  {
+    "revision": "ab86a6fd7f830ff58370",
+    "url": "/movie-project/static/js/2.5ab98c51.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/movie-project/static/js/2.5ab98c51.chunk.js.LICENSE"
+  },
+  {
+    "revision": "65b7accc5f31bbabf990",
+    "url": "/movie-project/static/js/main.01147720.chunk.js"
+  },
+  {
+    "revision": "48016625b8823da894d4",
+    "url": "/movie-project/static/js/runtime-main.3339472c.js"
+  }
+]);
